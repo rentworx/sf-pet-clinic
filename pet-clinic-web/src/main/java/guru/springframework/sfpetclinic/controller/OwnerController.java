32b1fecb4af0499e10e3142c24e2618a7f415938
Jim Kennedy <jgkennedy@rentworx.com>
2019-06-09 23:28:08 -0400

    Updated the owner index page to display the owners list.  Closes #16.

--- pet-clinic-web/src/main/java/guru/springframework/sfpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfpetclinic/controller/OwnerController.java
@@ -1,15 +1,25 @@
 package guru.springframework.sfpetclinic.controller;
 
+import guru.springframework.sfpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController
 {
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String ListVets()
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService)
     {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String ListVets(Model model)
+    {
+        model.addAttribute("owners", this.ownerService.findAll());
         return "owners/index";
     }
 }
