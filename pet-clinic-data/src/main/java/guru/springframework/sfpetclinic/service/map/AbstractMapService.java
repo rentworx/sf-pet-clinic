ce2f19f662f2fbc99493a26d694dedc1669041e7
Jim Kennedy <jgkennedy@rentworx.com>
2019-06-10 21:01:48 -0400

    Refactor AbstractMap so that Id generation is handled automatically.  Closes #23.

--- pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/service/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package guru.springframework.sfpetclinic.service.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sfpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID>
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long>
 {
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll()
     {
@@ -19,9 +18,20 @@ public abstract class AbstractMapService<T, ID>
         return map.get(id);
     }
 
-    T save(ID id, T object)
+    T save(T object)
     {
-        map.put(id, object);
+        if (object != null)
+        {
+            if (object.getId() == null)
+            {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }
+        else
+        {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
@@ -33,5 +43,19 @@ public abstract class AbstractMapService<T, ID>
     void delete(T object)
     {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId()
+    {
+        Long nextId = null;
+        try
+        {
+            nextId = Collections.max(map.keySet()) + 1;
+        }
+        catch (NoSuchElementException e)
+        {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
