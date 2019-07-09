73098455bff20c77ea55b1242fdb3ea50f4b0533
Jim Kennedy <jgkennedy@rentworx.com>
2019-07-08 23:42:42 -0400

    Implemented PetTypeServiceMap.  Closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/service/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfpetclinic.service;
+
+import guru.springframework.sfpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>
+{
+}
