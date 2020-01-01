384f3ed1baab154bd99c95748a371b566607bfa2
Jim Kennedy <jgkennedy@rentworx.com>
2020-01-01 14:21:43 -0500

    Updated base entity.  Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package guru.springframework.sfpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // targeting mySQL, database generates the ID
     private Long id;
 
     public Long getId()
