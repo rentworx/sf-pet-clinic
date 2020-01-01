8db39c07c7ac27531a5ca2f43ad3a069f30520f6
Jim Kennedy <jgkennedy@rentworx.com>
2020-01-01 14:41:29 -0500

    Owner class converted to JPA entity as well as dependent classes.  Closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/model/Person.java
@@ -1,8 +1,15 @@
 package guru.springframework.sfpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity
 {
-    private String firstName;
+    @Column(name = "first_name") // Just being explicit here
+    private String firstName;  // Hibernate by default will create columns in snake case
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName()
