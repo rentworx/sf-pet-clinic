f72841ebcf965a4882944affe2a138a9f2a209da
Jim Kennedy <jgkennedy@rentworx.com>
2020-01-05 12:32:54 -0500

    Created Visit map service. Closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/service/VisitService.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfpetclinic.service;
+
+import guru.springframework.sfpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>
+{
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/service/map/VisitMapService.java
@@ -0,0 +1,44 @@
+package guru.springframework.sfpetclinic.service.map;
+
+import guru.springframework.sfpetclinic.model.Visit;
+import guru.springframework.sfpetclinic.service.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService
+{
+    @Override
+    public Set<Visit> findAll()
+    {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id)
+    {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit)
+    {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+            || visit.getPet().getOwner().getId() == null)
+        {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit)
+    {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id)
+    {
+        super.deleteById(id);
+    }
+}
