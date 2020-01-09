ddd092f44424566c75340a47e92d7fe3242f4cfd
Jim Kennedy <jgkennedy@rentworx.com>
2020-01-09 15:45:29 -0500

    Check point.  Writing tests.

---
+++ buildapp.cmd
@@ -0,0 +1,2 @@
+@rem use the package task to generate css
+mvn package && java -jar target/pet-clinic-web-0.0.3-snapshot.jar
---
+++ pet-clinic-data/src/test/java/guru/springframework/sfpetclinic/model/PersonTest.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfpetclinic.model;
+
+import org.junit.Test;
+
+public class PersonTest
+{
+
+    @Test
+    public void getFirstName()
+    {
+    }
+
+    @Test
+    public void setFirstName()
+    {
+    }
+
+    @Test
+    public void getLastName()
+    {
+    }
+
+    @Test
+    public void setLastName()
+    {
+    }
+}
