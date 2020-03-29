261ea8e14bee51545d5da98fe537060221b631b7
Jim Kennedy <jgkennedy@rentworx.com>
2020-03-29 00:14:04 -0400

    Section 11, video 221.  Add visit complete. closes #57

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfpetclinic/controller/VisitControllerTest.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfpetclinic.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class VisitControllerTest
+{
+}
